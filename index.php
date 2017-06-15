6005ba46832151df48f67e7c33494b4c924646de
ulul <hudanuzulul@gmail.com>
2017-06-15 11:21:36 +0700

    Update index .php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 define('RESTRICTED',1);
-date_default_timezone_set("Asia/Bangkok");
+error_reporting(E_ALL);
 //Ensure that a session exists (just in case)
 if( !session_id() ) {
     session_start();
@@ -15,29 +15,13 @@ require('libraries/StoreValidation.php');
 
 // here our routes
 $page = (! empty($_GET['page'])) ? $_GET['page'] : null ;
-$action = (! empty($_GET['action'])) ? $_GET['action'] : null;
+
 
 switch ($page) {
 	case 'articles':
-		if ($action == 'create') {
-			require('articles/create.php');
-		} elseif($action == 'store') {
-			require('articles/store.php');
-		} elseif($action == 'createMultiple') {
-			require('articles/createMultiple.php');
-		} elseif($action == 'storemultiple') {
-			require('articles/storeMultiple.php');
-		} elseif ($action == 'edit') {
-			require('articles/edit.php');
-		} elseif ($action == 'update') {
-			require('articles/update.php');
-		} elseif ($action == 'delete') {
-			require('articles/delete.php');
-		} elseif ($action == 'detail') {
-			require('articles/detail.php');
-		} else {
-			require('articles/index.php');	
-		}
+		
+		require('articles/index.php');	
+		
 		break;
 	
 	default:
